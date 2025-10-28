fd93a3f6004c6375f28eb220354085546c7101f8
Eduardo Lizama <128654322+DaRaci-7@users.noreply.github.com>
2025-10-28 17:26:01 -0300

    docs:resumen.py se agrega archivo

---
+++ resumen.py
@@ -0,0 +1,9 @@
+def mostrar_resumen():
+    resultado = print (f"Nombre:{nombre}",f"edad: {edad}",f"cargo:{cargo}",f"sueldo:{sueldo}",f"Bono:{calcular_bono}", sep="\n")
+    return resultado
+
+
+          
+                
+
+    
